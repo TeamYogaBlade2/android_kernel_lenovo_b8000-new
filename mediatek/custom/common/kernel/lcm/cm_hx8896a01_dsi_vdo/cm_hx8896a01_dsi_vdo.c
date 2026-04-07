e4ac579ca52add1de11b084a95d7c431b5b56f29
Akari Tsuyukusa <akkun11.open@gmail.com>
2026-04-07 12:46:55 +0900

    add reversed lcm drivers
    Signed-off-by: Akari Tsuyukusa <akkun11.open@gmail.com>

--- mediatek/custom/common/kernel/lcm/cm_hx8896a01_dsi_vdo/cm_hx8896a01_dsi_vdo.c
+++ mediatek/custom/common/kernel/lcm/cm_hx8896a01_dsi_vdo/cm_hx8896a01_dsi_vdo.c
@@ -1,5 +1,5 @@
 #include <linux/string.h>
-
+#include <platform/mt_gpio.h>
 #include "lcm_drv.h"
 
 
@@ -32,63 +32,95 @@ static LCM_UTIL_FUNCS lcm_util = {0};
 //  LCM Driver Implementations
 // ---------------------------------------------------------------------------
 
-static void lcm_set_util_funcs(const LCM_UTIL_FUNCS *util)
+static void lcd_power_en(void)
 {
-    memcpy(&lcm_util, util, sizeof(LCM_UTIL_FUNCS));
+  hwPowerOn(0x12,0x708,"LCM");
+  mt_set_gpio_mode(0x89,0);
+  mt_set_gpio_dir(0x89,1);
+  mt_set_gpio_out(0x89,1);
+  MDELAY(0x78);
+  return;
 }
 
+
+static bool lcm_compare_id(void)
+{
+  uint uVar1;
+  int iVar2;
+
+  lcd_power_en();
+  MDELAY(10);
+  mt_set_gpio_mode(0x9a,0);
+  mt_set_gpio_dir(0x9a,0);
+  uVar1 = mt_get_gpio_in(0x9a);
+  printk("blade10 lcd_id cmi  %d \n",uVar1);
+  iVar2 = 1 - uVar1;
+  if (1 < uVar1) {
+    iVar2 = 0;
+  }
+  return iVar2;
+}
 
 static void lcm_get_params(LCM_PARAMS *params)
 {
-    memset(params, 0, sizeof(LCM_PARAMS));
-
-    params->type   = LCM_TYPE_DPI;
-    params->ctrl   = LCM_CTRL_NONE;
-    params->width  = FRAME_WIDTH;
-    params->height = FRAME_HEIGHT;
-    
-    params->dpi.mipi_pll_clk_ref  = 0;
-    params->dpi.mipi_pll_clk_div1 = 42;
-    params->dpi.mipi_pll_clk_div2 = 10;
-    params->dpi.dpi_clk_div       = 2;
-    params->dpi.dpi_clk_duty      = 1;
-
-    params->dpi.clk_pol           = LCM_POLARITY_FALLING;
-    params->dpi.de_pol            = LCM_POLARITY_FALLING;
-    params->dpi.vsync_pol         = LCM_POLARITY_FALLING;
-    params->dpi.hsync_pol         = LCM_POLARITY_FALLING;
-
-    params->dpi.hsync_pulse_width = 4;
-    params->dpi.hsync_back_porch  = 10;
-    params->dpi.hsync_front_porch = 18;
-    params->dpi.vsync_pulse_width = 2;
-    params->dpi.vsync_back_porch  = 2;
-    params->dpi.vsync_front_porch = 14;
-    
-    params->dpi.format            = LCM_DPI_FORMAT_RGB888;
-    params->dpi.rgb_order         = LCM_COLOR_ORDER_RGB;
-    params->dpi.is_serial_output  = 0;
-
-    params->dpi.intermediat_buffer_num = 2;
-
-    params->dpi.io_driving_current = 0;
+  __memzero(params,0x23c);
+  params->type = LCM_TYPE_DSI;
+  (params->dsi).mode = SYNC_EVENT_VDO_MODE;
+  (params->dsi).LANE_NUM = LCM_FOUR_LANE;
+  (params->dsi).data_format.color_order = LCM_COLOR_ORDER_ RGB;
+  (params->dsi).data_format.trans_seq = LCM_DSI_TRANS_SEQ_ MSB_FIRST;
+  (params->dsi).data_format.padding = LCM_DSI_PADDING_ON_L SB;
+  (params->dsi).data_format.format = LCM_DSI_FORMAT_RGB888 ;
+  (params->dsi).intermediat_buffer_num = 0;
+  (params->dsi).PS = LCM_PACKED_PS_24BIT_RGB888;
+  (params->dsi).vertical_sync_active = 2;
+  params->width = 0x500;
+  (params->dsi).vertical_backporch = 10;
+  params->height = 800;
+  (params->dsi).vertical_frontporch = 0x15;
+  (params->dsi).packet_size = 0x100;
+  (params->dsi).horizontal_sync_active = 5;
+  (params->dsi).word_count = 0x870;
+  (params->dsi).horizontal_backporch = 0x32;
+  (params->dsi).vertical_active_line = 800;
+  (params->dsi).horizontal_frontporch = 0xba;
+  (params->dsi).horizontal_active_pixel = 0x500;
+  (params->dsi).PLL_CLOCK = 6;
+  return;
 }
-
 
 static void lcm_init(void)
 {
-}
-
-
-static void lcm_suspend(void)
-{
+  lcd_power_en();
+  return;
 }
 
 
 static void lcm_resume(void)
 {
+  lcd_power_en();
+  MDELAY(100);
+  return;
 }
 
+
+static void lcm_set_util_funcs(LCM_UTIL_FUNCS *util)
+{
+  memcpy(&lcm_util, util, sizeof(LCM_UTIL_FUNCS));
+  return;
+}
+
+
+static void lcm_suspend(void)
+{
+  MDELAY(200);
+  MDELAY(0x78);
+  mt_set_gpio_mode(0x89,0);
+  mt_set_gpio_dir(0x89,1);
+  mt_set_gpio_out(0x89,0);
+  hwPowerDown(0x12,"LCM");
+  return;
+}
 
 // ---------------------------------------------------------------------------
 //  Get LCM Driver Hooks
@@ -96,9 +128,12 @@ static void lcm_resume(void)
 
 LCM_DRIVER cm_hx8896a01_dsi_vdo_drv =
 {
-        .set_util_funcs = lcm_set_util_funcs,
-        .get_params     = lcm_get_params,
-        .init           = lcm_init,
-        .suspend        = lcm_suspend,
-        .resume         = lcm_resume
+	.name = "cm_hx8896a01_dsi_vdo",
+	.lcd_power_en = lcd_power_en,
+	.lcm_compare_id = lcm_compare_id,
+	.lcm_get_params = lcm_get_params,
+	.lcm_init = lcm_init,
+	.lcm_resume = lcm_resume,
+	.lcm_set_util_funcs = lcm_set_util_funcs,
+	.lcm_suspend = lcm_suspend,
 };
